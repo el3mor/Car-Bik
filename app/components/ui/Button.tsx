59cfa491cfeb7995af5f07ece25fd59f375726f5
el3mor <elamoor82@gmail.com>
2025-02-05 20:09:29 +0200

    new edit

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,9 +1,9 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import { getData } from '@/app/hooks/getData'
 
-
-const Button = ({
+const Button = async ({
   className,
   withMsg = false,
   msgInput
@@ -13,8 +13,10 @@ const Button = ({
   msgInput?: string
 
 }) => {
+  const data = await getData()
+  const phoneNumber = data?.['whatsapp-phone']
+
   const msg = encodeURIComponent(msgInput || 'أرغب في حجز موعد لخدمة تلميع السيارة')
-  const phoneNumber = '055 555 5555'
   return (
     <Link className={`${className} flex items-center justify-center px-6 py-3 rounded-md w-[300px] duration-300`}
       href={`https://wa.me/${phoneNumber}?${withMsg ? `text=${msg}` : ''}`}
