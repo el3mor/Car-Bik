2ecacb4dc466cd72e6038111b1ee8b5155a240cd
el3mor <elamoor82@gmail.com>
2025-02-06 07:26:32 +0200

    fix phoneNum

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,9 +1,9 @@
 "use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { getData } from '@/app/hooks/getData'
 
-const Button = async ({
+const Button = ({
   className,
   withMsg = false,
   msgInput
@@ -13,13 +13,17 @@ const Button = async ({
   msgInput?: string
 
 }) => {
-  const data = await getData()
-  const phoneNumber = data?.['whatsapp-phone']
+  const [phoneNum, setPhoneNum] = React.useState('')
+  useEffect(() => {
+    getData().then((data) => {
+      setPhoneNum(data["whatsapp-phone"])
+    })
+  },[])
 
   const msg = encodeURIComponent(msgInput || 'أرغب في حجز موعد لخدمة تلميع السيارة')
   return (
     <Link className={`${className} flex items-center justify-center px-6 py-3 rounded-md w-[300px] duration-300`}
-      href={`https://wa.me/${phoneNumber}?${withMsg ? `text=${msg}` : ''}`}
+      href={`https://wa.me/${phoneNum}?${withMsg ? `text=${msg}` : ''}`}
       target="_blank"
       ref={ref => ref?.setAttribute('rel', 'noopener noreferrer')}
     >
