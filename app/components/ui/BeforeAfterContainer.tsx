9be47124a3924c280ddb29ac33db5195720e9a28
el3mor <elamoor82@gmail.com>
2025-02-19 02:16:18 +0200

    fix responesive

--- app/components/ui/BeforeAfterContainer.tsx
+++ app/components/ui/BeforeAfterContainer.tsx
@@ -9,7 +9,7 @@ const BeforeAfterContainer = ({
   after: string
 }) => {
   return (
-    <div className='flex gap-10 items-center justify-evenly'>
+    <div className='flex max-md:flex-col gap-10 items-center justify-evenly'>
       <div className='flex flex-col gap-4 items-center justify-center  '>
         <div className='relative w-[600px] h-[600px] rounded-2xl overflow-hidden'>
           <Image src={before} alt='Before' layout='fill' objectFit='cover' />
