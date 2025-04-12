1f6b3e86c42975896cda5a6d99a27a8d8f210d1c
el3mor <el3mor>
2025-04-12 16:05:16 +0200

    edits

--- app/components/ui/ContactIcon.tsx
+++ app/components/ui/ContactIcon.tsx
@@ -5,14 +5,14 @@ import { IoCall } from "react-icons/io5";
 
 const ContactIcon = ({phone}: {phone:string}) => {
   return (
-    <div className="fixed bottom-2 left-2 z-[60] flex flex-col gap-4  items-center justify-center">
+    <div className="fixed bottom-2 right-2 z-[60] flex flex-col gap-4  items-center justify-center">
       <Link
         href={`https://wa.me/${phone}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex h-10 w-10  md:h-8 md:w-8 items-center justify-center rounded-full bg-green-500 text-white  shadow-lg transition duration-300 ease-in-out hover:bg-green-600"
+        className="flex h-10 w-10  md:h-12 md:w-12 items-center justify-center rounded-full bg-green-500 text-white  shadow-lg transition duration-300 ease-in-out hover:bg-green-600"
       >
-        <FaWhatsapp size={27} />
+        <FaWhatsapp size={32} />
       </Link>
       <Link
         href={`tel:${phone}`}
